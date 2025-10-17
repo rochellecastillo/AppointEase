672253352c18f026744f09bfbe528d1936a0fddf
Rochelle Castillo <castillorochelle27@gmail.com>
2025-10-17 08:29:47 +0800

    update class

--- Class/Appointment.php
+++ Class/Appointment.php
@@ -1,6 +1,8 @@
 <?php
 require_once'Database.php';
 Class Appointment extends Database{
-
+    public function displayappointment($date){
+        $sql="select * from tblappointment where bookingdate=?";
+    }
 }
 ?>
--- Class/User.php
+++ Class/User.php
@@ -16,5 +16,9 @@ Class User extends Database{
             return false;
         }
     }
+    public function displayuserinfo($userid){
+        $sql="select i.*,u.user_name,u.password,u.user_type from tblinfo i inner join tbluser u on i.user_id =u.user_id where i.user_id=?";
+
+    }
 }
 ?>
