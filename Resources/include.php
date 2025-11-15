5cc049b5c761b9b0d8d70245ae5dacad05516dbe
Rochelle Castillo <castillorochelle27@gmail.com>
2025-11-15 19:27:47 +0800

    remove offline resources

--- Resources/include.php
+++ Resources/include.php
@@ -1,6 +1,9 @@
-<link rel="stylesheet" href="/AppointEase/Resources/bootstrap-5.3.8/dist/css/bootstrap.min.css">
+<!--link rel="stylesheet" href="/AppointEase/Resources/bootstrap-5.3.8/dist/css/bootstrap.min.css">
 <script src="/AppointEase/Resources/bootstrap-5.3.8/dist/js/bootstrap.bundle.min.js"></script>
-<link rel="stylesheet" href="/AppointEase/Resources/fontawesome-free-7.0.1/css/all.css">
+<link rel="stylesheet" href="/AppointEase/Resources/fontawesome-free-7.0.1/css/all.css"-->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <script src="/AppointEase/Resources/sweetalert2@11.js"></script>
 <link rel="icon" type="image/png" sizes="16x16" href="Resources/Images/logo.png" />
 <script src="/AppointEase/Resources/jquery-3.7.1.min.js"></script>
